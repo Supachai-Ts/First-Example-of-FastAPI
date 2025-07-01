f2a3f787b2f4ae02249afaca8479b6dc706ac46a
Supachai_280246 <6510110469@email.psu.ac.th>
2025-07-01 19:30:11 +0700

    การใช้ Put API and Delete API

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ app = FastAPI()
 
 class Item(BaseModel):
     name: str
-    description: str
     price: float
 
 @app.get("/hello")
@@ -18,6 +17,14 @@ def hello_world():
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
-@app.post("/items")
+@app.post("/items/")
 def create_item(item: Item):
     return {"request body": item}
+
+app.put("/items/{item_id}")
+def edit_item(item_id: int, item: Item):
+    return {"id":item_id, "request body": item}
+
+app.delete("/items/{item_id}")
+def delete_item(item_id: int):
+    return {"message": f"Item {item_id} deleted"}
