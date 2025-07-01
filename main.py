fdf72165d60dd0e8e218b1795bf8c4dc7f5a7580
Supachai_280246 <6510110469@email.psu.ac.th>
2025-07-01 18:53:48 +0700

    Post API กับการรับ request

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
-@app.get("/")
-
+@app.get("/hello")
 def hello_world():
     return {"message": "Hello World"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+@app.post("/items")
+async def create_item(request: Request):
+    body = await request.json()
+    return {"request body": body}
