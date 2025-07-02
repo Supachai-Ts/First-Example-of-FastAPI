b62c4e721763c186aa8e09d6acbe08ff8b48a1a7
Supachai_280246 <6510110469@email.psu.ac.th>
2025-07-02 20:41:01 +0700

    Create Database Model (SQLAlchemy)

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from typing import Union
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, Integer, String
 
 from fastapi import FastAPI
 from pydantic import BaseModel
@@ -12,8 +13,20 @@ SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush= False, bind=engine)
 Base = declarative_base()
+
+# Step 2: ORM class
+class ItemDB(Base):
+    __tablename__ = "items"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, index=True)
+    description = Column(String, index=True)
+    price = Column(Integer)
+
+# Create Database
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
