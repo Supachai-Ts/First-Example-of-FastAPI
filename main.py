2d37569df90e8097b5ee2836526d0ab035c6ae38
Supachai_280246 <6510110469@email.psu.ac.th>
2025-07-02 20:05:58 +0700

    Database ORM Config

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 from typing import Union
 
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
 from fastapi import FastAPI
 from pydantic import BaseModel
+
+# Step 1: Create a SQLAlchemy engine
+SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
 
 app = FastAPI()
 
 class Item(BaseModel):
     name: str
     price: float
-
-@app.get("/hello")
-def hello_world():
-    return {"message": "Hello World"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
